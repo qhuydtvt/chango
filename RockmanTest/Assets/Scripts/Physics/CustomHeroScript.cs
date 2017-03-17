f7d439e2d56afdc52f664828384337290aa81100
huynq <qhuydtvt@gmail.com>
2017-03-18 03:36:57 +0700

    First commit

---
+++ RockmanTest/Assets/Scripts/Physics/CustomHeroScript.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class CustomHeroScript : MonoBehaviour
+{
+	public int numberOfRayHorizontal;
+	public int numberOfRayVertical;
+
+	private float raySpacingHorizontal;
+	private float raySpacingVertical;
+
+	public LayerMask collisionMask;
+
+	private BoxCollider2D coll;
+	private Bounds colliderBounds;
+
+
+	private float skinWidth = 0.1f;
+
+	private RaycastOrigins raycastOrigins;
+	public CollisionInfo collisionInfo;
+
+	private void Awake() {
+		coll = GetComponent<BoxCollider2D> ();
+		CalculateBounds ();
+		CalculateRaySpacing ();
+	}
+
+	private void CalculateBounds() {
+		colliderBounds = coll.bounds;
+		colliderBounds.Expand (-skinWidth * 2);
+
+		raycastOrigins = new RaycastOrigins ();
+		raycastOrigins.topLeft = new Vector2 (colliderBounds.min.x, colliderBounds.max.y);
+		raycastOrigins.topRight = new Vector2 (colliderBounds.max.x, colliderBounds.max.y);
+		raycastOrigins.bottomLeft = new Vector2 (colliderBounds.min.x, colliderBounds.min.y);
+		raycastOrigins.bottomRight = new Vector2 (colliderBounds.max.x, colliderBounds.min.y);
+	}
+
+	private void CalculateRaySpacing() {
+		if (numberOfRayHorizontal < 3) {
+			numberOfRayHorizontal = 3;
+		}
+
+		if (numberOfRayVertical < 3) {
+			numberOfRayVertical = 3;
+		}
+
+		raySpacingVertical = (colliderBounds.extents.x * 2) / (numberOfRayVertical - 1);
+		raySpacingHorizontal = ((colliderBounds.extents.y * 2) / (numberOfRayHorizontal - 1));
+	}
+
+	public void Move(Vector2 velocity) {
+		CalculateBounds ();
+		collisionInfo.Reset ();
+
+		if (velocity.y != 0) {
+			RaycastVertical (ref velocity);
+		}
+
+		if(velocity.x != 0)
+		{
+			RaycastHorizontal(ref velocity);
+		}
+
+		transform.position += (Vector3)velocity;
+	}
+
+	private void RaycastHorizontal(ref Vector2 velocity)
+	{
+		float direction = Mathf.Sign(velocity.x);
+		float distance = Mathf.Abs(velocity.x);
+		Vector2 raycastBase = direction < 0 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+
+		for(int i=0; i < numberOfRayHorizontal; i++)
+		{
+			Vector2 raycastOrigin = raycastBase + raySpacingHorizontal * i * Vector2.up;
+
+			RaycastHit2D[] hits = 
+				Physics2D.RaycastAll(raycastOrigin, new Vector2(direction, 0), distance + skinWidth, collisionMask);
+
+			Debug.DrawRay(raycastOrigin, new Vector2(velocity.x, 0), Color.green, Time.fixedDeltaTime, false);
+
+			foreach(RaycastHit2D hit in hits)
+			{
+				if (hit.collider.isTrigger)
+				{
+					hit.collider.SendMessage("OnTriggerEnter2D", coll);
+				}
+				else
+				{
+					if (Mathf.Abs(velocity.x) > (hit.distance - skinWidth) * direction)
+					{
+						velocity.x = (hit.distance - skinWidth) * direction;
+					}
+					if (direction > 0) {
+						// Right
+						collisionInfo.collideRightArr[i] = true;
+					} else {
+						// Left
+						collisionInfo.collideLeftArr[i] = true;
+					}
+				}
+			}
+		}
+	}
+
+	private void RaycastVertical(ref Vector2 velocity) {
+		float direction = Mathf.Sign(velocity.y);
+		float distance = Mathf.Abs(velocity.y);
+		Vector2 raycastBase = direction < 0 ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+
+		for (int i = 0; i < numberOfRayVertical; i++)
+		{
+			Vector2 raycastOrigin = raycastBase + raySpacingVertical * i * Vector2.right;
+
+			RaycastHit2D[] hits =
+				Physics2D.RaycastAll(raycastOrigin, new Vector2(0, direction), distance + skinWidth, collisionMask);
+			
+			Debug.DrawRay(raycastOrigin, new Vector2(0, velocity.y * 10), Color.yellow, Time.fixedDeltaTime, false);
+
+			foreach (RaycastHit2D hit in hits)
+			{
+				if (hit.collider.isTrigger)
+				{
+					hit.collider.SendMessage("OnTriggerEnter2D", coll);
+				}
+				else
+				{
+					if(Mathf.Abs(velocity.y) > (hit.distance - skinWidth) * direction)
+					{
+						velocity.y = (hit.distance - skinWidth) * direction;
+					}
+					if (direction > 0) {
+						// Top
+						collisionInfo.collideTopArr[i] = true;
+					} else {
+						//Bottom
+						collisionInfo.collideBottomArr[i] = true;
+					}
+				}
+			}
+		}
+	}
+
+}
+
+public struct RaycastOrigins {
+	public Vector2 topLeft, topRight;
+	public Vector2 bottomLeft, bottomRight;
+}
+
+public struct CollisionInfo
+{
+	public bool[] collideTopArr;
+	public bool[] collideBottomArr;
+	public bool[] collideRightArr;
+	public bool[] collideLeftArr;
+
+	public void Reset()
+	{
+		collideTopArr = new bool[] { false, false, false };
+		collideBottomArr = new bool[] { false, false, false };
+		collideLeftArr = new bool[] { false, false, false };
+		collideRightArr = new bool[] { false, false, false };
+	}
+
+	public bool collideBottom {
+		get {
+			return collideBottomCount > 0;
+		}
+	}
+
+	public int collideBottomCount {
+		get {
+			return this.collideBottomArr.Count (item => item);
+		}
+	}
+
+	public bool collideTop {
+		get {
+			return collideTopCount > 0;
+		}
+	}
+
+	public int collideTopCount {
+		get {
+			return collideTopArr.Count (item => item);
+		}
+	}
+
+	public bool collideRight {
+		get {
+			return collideRightCount > 0;
+		}
+	}
+
+	public int collideRightCount {
+		get {
+			return collideRightArr.Count (item => item);
+		}
+	}
+
+	public bool collideLeft {
+		get {
+			return collideLeftCount > 0;
+		}
+	}
+
+	public int collideLeftCount {
+		get {
+			return collideLeftArr.Count (item => item);
+		}
+	}
+}
---
+++ RockmanTest/Assets/Scripts/Physics/HeroScript.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CustomHeroScript))]
+public class HeroScript : MonoBehaviour {
+
+	public Vector2 NatureGravity = new Vector2 (0, -0.983f);
+	public int FixedTimeMulti = 100;
+
+	public Vector2 CounterGravity = new Vector2(0, 0);
+
+	public CustomHeroScript CustomHeroScript;
+
+	private Vector2 currentVelocity = new Vector2(0.0f, 0.0f);
+
+	private bool doubleJumpEnabled = true;
+
+	private InputInfo inputInfo;
+
+	private int dashCounter = 0;
+	private bool dashable = true;
+	private int dashableCoolDownCounter = 0;
+
+	private int hoverCounter = 0;
+	private bool hoverable = true;
+	private int hoverCoolDownCounter = 0;
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+
+	private void processInput() {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			inputInfo.Left = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			inputInfo.Left = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			inputInfo.Right = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			inputInfo.Right = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			inputInfo.Up = true;
+		} 
+		else if (Input.GetKeyUp(KeyCode.UpArrow)) {
+//			inputInfo.Up = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.C)) {
+			inputInfo.C = true;
+		} 
+		else if (Input.GetKeyUp(KeyCode.C)) {
+			//			inputInfo.Up = false;
+		}
+
+//		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+//			inputInfo.Down = true;
+//		}
+//		else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+//			inputInfo.Down = false;
+//		}
+	}
+
+	private void Update() {
+		processInput ();
+
+		if(Input.GetKeyDown(KeyCode.X) && dashable) {
+			inputInfo.X = true;
+			dashCounter = 0;
+			dashable = false;
+		}
+
+		if (inputInfo.X) {
+			dashCounter++;
+			if (dashCounter > 20) {
+				inputInfo.X = false;
+			}
+		}
+
+		if (!dashable) {
+			dashableCoolDownCounter++;
+			if (dashableCoolDownCounter > 120) {
+				dashable = true;
+				dashableCoolDownCounter = 0;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			if (CustomHeroScript.collisionInfo.collideBottom) {
+				currentVelocity.y = 0.3f;
+			} else if (doubleJumpEnabled) {
+				currentVelocity.y = 0.3f;
+				doubleJumpEnabled = false;	
+			}
+		}
+		else if (CustomHeroScript.collisionInfo.collideBottom) {
+			currentVelocity.y = 0;
+			doubleJumpEnabled = true;
+		}
+
+		if (CustomHeroScript.collisionInfo.collideRightCount > 1 && inputInfo.Right) {
+			print ("Stick right");
+			CounterGravity = NatureGravity * -1;
+			currentVelocity.y = -0.05f;
+		} else {
+			CounterGravity.Set (0, 0);
+		}
+
+		if (hoverable) {
+			
+		}
+
+		if (hoverable && Input.GetKeyDown (KeyCode.C)) {
+			inputInfo.C = true;
+			hoverCounter = 0;
+			hoverable = false;
+		}
+
+		if (inputInfo.C) {
+			CounterGravity = NatureGravity * -0.8f;
+			currentVelocity.y = -0.05f;
+
+			hoverCounter++;
+			if (hoverCounter > 120) {
+				inputInfo.C = false;
+			}
+		}
+
+		if (!hoverable) {
+			hoverCoolDownCounter++;
+			if (hoverCoolDownCounter > 180) {
+				hoverable = true;
+				hoverCoolDownCounter = 0;
+			}
+		}
+
+		if (CustomHeroScript.collisionInfo.collideLeftCount > 1 && inputInfo.Left) {
+			print ("Stick left");
+			CounterGravity = NatureGravity * -1;
+			currentVelocity.y = -0.05f;
+		} else {
+			CounterGravity.Set (0, 0);
+		}
+
+		calculateMovementVector (inputInfo, ref currentVelocity);
+
+	}
+
+	private void FixedUpdate() {
+
+		currentVelocity += (NatureGravity + CounterGravity) / FixedTimeMulti;
+
+		CustomHeroScript.Move (currentVelocity);
+	}
+
+	private void calculateMovementVector(InputInfo inputInfo, ref Vector2 movementVector) {
+
+		movementVector.x = 0;
+
+		if(inputInfo.Left && !inputInfo.X) {
+			movementVector.x = -0.2f;	
+		}
+
+		if (inputInfo.Right && !inputInfo.X) {
+			movementVector.x = 0.2f;
+		}
+
+		if (inputInfo.Left && inputInfo.X) {
+			movementVector.x = -0.4f;
+		}
+
+		if (inputInfo.Right && inputInfo.X) {
+			movementVector.x = 0.4f;
+		}
+	}
+
+	struct InputInfo {
+		public bool Up, Down, Left, Right;
+		public bool X, Z, C;
+
+		public void Reset() {
+			Up = Down = Left = Right = false;
+			X = Z = C = false;
+		}
+	}
+}
